cdda087fc63491e3a5a27d52a4f6aaf742b492d3
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-06-13 15:57:09 -0700

    keep forgetting to commit, trying to get slogans to work

--- src/App.js
+++ src/App.js
@@ -9,43 +9,33 @@ import CityNameInput from './CityNameInput';
 import City from './City';
 
 function App() {
-  // track some state here.
-  // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
   const [skylineId, setSkyline] = useState(1);
   const [waterfrontId, setWaterfront] = useState(1);
   const [castleId, setCastle] = useState(1);
-  // you'll need to track a city name, which starts as the city name of your choice.
-  const [CityNameInput, setCityName] = useState('Portland');
-  // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
+  const [CityName, setCityName] = useState('Portland');
   const [slogans, setSlogans] = useState(['The City of Beer']);
 
   return (
     <div className="App">
-      {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
-      <SkylineDropdown setSkyline={setSkyline} />
-      <WaterfrontDropdown setWaterfront={setWaterfront} />
-      <CastleDropdown setCastle={setCastle} />
+      <City
+        skylineId={skylineId} 
+        waterfrontId={waterfrontId}
+        castleId={castleId}
+      />
       <h1>
-        {/* dynamically update the city name here using state */}
-        Welcome to beautiful Portland!
+        Welcome to {CityName}!
       </h1>
       <div className='bottom'>
-        {/* here, the CityNameInput component takes in the setCityName state handler function */}
+        <CityNameInput setCityName={setCityName} />
         <section className='dropdowns'>
-          {/* 
-          render all three Dropdown components (WaterfrontDropdown, SkylineDropdown, CastleDropdown) here. 
-          
-          In each Dropdown, pass as props the state handler functions defined above, so that these child components can manipulate parent state 
-          */}
-          <City
-            skylineId={skylineId} 
-            waterfrontId={waterfrontId}
-            castleId={castleId}
-          />
+          <SkylineDropdown setSkyline={setSkyline} />
+          <WaterfrontDropdown setWaterfront={setWaterfront} />
+          <CastleDropdown setCastle={setCastle} />
         </section>
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
+        <SloganForm setSlogans={setSlogans} />
         {/* here, the SloganList component takes the array of slogans that lives in state */}
-
+        <SloganList />
       </div>
     </div>
   );
